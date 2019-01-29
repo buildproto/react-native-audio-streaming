bd83002858fd09c7c63688d60888b7c2393d3381
Andrew Roper <andrewroper@gmail.com>
2019-01-28 20:54:47 -0800

    add method to silence warning

--- ios/ReactNativeAudioStreaming.m
+++ ios/ReactNativeAudioStreaming.m
@@ -27,6 +27,11 @@ RCT_EXPORT_MODULE()
    return dispatch_get_main_queue();
 }
 
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
 - (ReactNativeAudioStreaming *)init
 {
    self = [super init];
